30080faceb6dff8af30739430cf18c9d26201a2d
Serban Mihai Radu <M.R.Serban-1@student.tudelft.nl>
2023-07-19 19:39:28 +0300

    skeleton common classes

--- src/main/java/me/raduserban11/commons/Playlist.java
+++ src/main/java/me/raduserban11/commons/Playlist.java
@@ -1,4 +1,36 @@
 package me.raduserban11.commons;
 
+import java.util.List;
+import java.util.Objects;
+
 public class Playlist {
+
+    private String name;
+    private List<Song> songs;
+
+    public Playlist(String name, List<Song> songs) {
+        this.name = name;
+        this.songs = songs;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Song> getSongs() {
+        return songs;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Playlist playlist = (Playlist) o;
+        return Objects.equals(name, playlist.name) && Objects.equals(songs, playlist.songs);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, songs);
+    }
 }
--- src/main/java/me/raduserban11/commons/Song.java
+++ src/main/java/me/raduserban11/commons/Song.java
@@ -1,4 +1,35 @@
 package me.raduserban11.commons;
 
+import java.util.Objects;
+
 public class Song {
+
+    private String name;
+    private String artist;
+
+    public Song(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Song song = (Song) o;
+        return Objects.equals(name, song.name) && Objects.equals(artist, song.artist);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, artist);
+    }
 }
