dd14665c9d327ed4e8933178096f83974995b3d5
Serban Mihai Radu <M.R.Serban-1@student.tudelft.nl>
2023-07-20 19:58:45 +0300

    almost well-working version

--- src/main/java/me/raduserban11/Downloader.java
+++ src/main/java/me/raduserban11/Downloader.java
@@ -12,14 +12,18 @@ import java.time.Duration;
 
 public class Downloader {
 
-    public static void download(Playlist playlist){
+    public int songsDownloaded = 0;
+
+    public void download(Playlist playlist){
         WebDriver driver = new ChromeDriver();
 
         for(Song song : playlist.getSongs()){
             String ytUrl = "https://www.youtube.com/results?search_query=" + song.getName().replace(" " ,"+" );
             driver.get(ytUrl);
-            WebElement acceptButton = driver.findElement(By.xpath("/html/body/ytd-app/ytd-consent-bump-v2-lightbox/tp-yt-paper-dialog/div[4]/div[2]/div[6]/div[1]/ytd-button-renderer[2]/yt-button-shape/button"));
-            acceptButton.click();
+            if(songsDownloaded == 0) {
+                WebElement acceptButton = driver.findElement(By.xpath("/html/body/ytd-app/ytd-consent-bump-v2-lightbox/tp-yt-paper-dialog/div[4]/div[2]/div[6]/div[1]/ytd-button-renderer[2]/yt-button-shape/button"));
+                acceptButton.click();
+            }
             driver.navigate().refresh();
             WebElement thumbnail = new WebDriverWait(driver, Duration.ofSeconds(2))
                     .until(d -> d.findElement(By.xpath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[1]/div[1]/ytd-thumbnail/a")));
@@ -33,6 +37,7 @@ public class Downloader {
                     .until(d -> d.findElement(By.xpath("/html/body/form/div[2]/a[1]")));
             String downloadLink = downloadButton.getAttribute("href");
             driver.get(downloadLink);
+            songsDownloaded++;
         }
     }
 }
--- src/main/java/me/raduserban11/Main.java
+++ src/main/java/me/raduserban11/Main.java
@@ -1,7 +1,23 @@
 package me.raduserban11;
 
+import me.raduserban11.commons.Playlist;
+import me.raduserban11.utils.TextToPlaylistConverter;
+
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please input playlist text file path:");
+        String path = scanner.nextLine();
+
+        if(path.startsWith("\"") && path.endsWith("\""))
+            path = path.replaceFirst("\"", "").substring(0, path.length() - 2);
+
+        Playlist playlist = TextToPlaylistConverter.convert(new File(path));
+        Downloader downloader =  new Downloader();
+        downloader.download(playlist);
     }
 }
