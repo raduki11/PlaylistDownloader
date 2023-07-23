c1f1531babf99b1840628e1c210423273df86961
Serban Mihai Radu <M.R.Serban-1@student.tudelft.nl>
2023-07-23 21:54:47 +0300

    tested song class

---
+++ src/test/java/me/raduserban11/commons/SongTest.java
@@ -0,0 +1,38 @@
+package me.raduserban11.commons;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SongTest {
+
+    Song song1;
+
+    @BeforeEach
+    void setUp(){
+        song1 =  new Song("apricots", "bicep");
+    }
+
+    @Test
+    void getName() {
+        assertEquals(song1.getName(), "apricots");
+    }
+
+    @Test
+    void getArtist() {
+        assertEquals(song1.getArtist(), "bicep");
+    }
+
+    @Test
+    void testEquals() {
+        Song song2 = new Song("apricots", "bicep");
+        assertEquals(song1, song2);
+    }
+
+    @Test
+    void testHashCode() {
+        Song song2 = new Song("apricots", "bicep");
+        assertEquals(song1.hashCode(), song2.hashCode());
+    }
+}
